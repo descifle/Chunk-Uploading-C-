c27018c028f98472acf30296309ecd2222ff7fe4
Giovanni Headley <descifle_itb@yahoo.com>
2024-02-01 18:25:19 -0500

    Remove extra fluff

--- TestChunkUpload/Exception/NotFoundException.cs
+++
@@ -1,9 +0,0 @@
-namespace ChunkUpload.Exception
-{
-    public class NotFoundException : ApiException
-    {
-        public NotFoundException(string msg) : base(404, msg)
-        {
-        }
-    }
-}

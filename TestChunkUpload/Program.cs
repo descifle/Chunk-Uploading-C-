d146ade92af5b96b7302caf7cdbff149bcf26392
Giovanni Headley <descifle_itb@yahoo.com>
2024-02-01 18:37:35 -0500

    cleanup to simplify

--- TestChunkUpload/Program.cs
+++ TestChunkUpload/Program.cs
@@ -3,8 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddSwaggerGen();
-
 builder.Services.Configure<IISServerOptions>(options =>
 {
     options.AllowSynchronousIO = true;
@@ -31,6 +29,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.UseCors(policy => policy.AllowAnyOrigin());
+app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
 
 app.Run();
